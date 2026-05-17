ba5cd395668b0346fdf63eb26571408bfafc66e2
shivamgravity <shivamgravity@gmail.com>
2026-05-17 04:54:25 +0000

    created files inside api/routes

---
+++ backend/app/api/routes/upload.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, UploadFile, File
+import os
+
+router = APIRouter()
+
+UPLOAD_DIR = "uploads"
+
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+@router.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
+
+    with open(file_path, "wb") as buffer:
+        buffer.write(await file.read())
+
+    return {
+        "filename": file.filename,
+        "message": "File uploaded successfully"
+    }
