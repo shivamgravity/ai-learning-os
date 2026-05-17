777bbb5873d78a3417841ed6061dd2d84d35ce0f
shivamgravity <shivamgravity@gmail.com>
2026-05-17 04:38:12 +0000

    created dashboard

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,15 @@
+function Dashboard() {
+  return (
+    <div>
+      <h2 className="text-4xl font-bold">
+        Welcome to AI Learning OS
+      </h2>
+
+      <p className="mt-4 text-zinc-400">
+        Your offline AI-powered learning companion.
+      </p>
+    </div>
+  );
+}
+
+export default Dashboard;
