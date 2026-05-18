99b8affb878a0df7217fc624258654b93da58d8f
shivamgravity <shivamgravity@gmail.com>
2026-05-18 08:17:49 +0000

    added text chunking code

---
+++ backend/app/services/chunking_service.py
@@ -0,0 +1,23 @@
+from typing import List
+
+def chunk_text(
+    text: str,
+    chunk_size: int = 500,
+    overlap: int = 100
+) -> List[str]:
+
+    chunks = []
+
+    start = 0
+
+    while start < len(text):
+
+        end = start + chunk_size
+
+        chunk = text[start:end]
+
+        chunks.append(chunk)
+
+        start += chunk_size - overlap
+
+    return chunks
